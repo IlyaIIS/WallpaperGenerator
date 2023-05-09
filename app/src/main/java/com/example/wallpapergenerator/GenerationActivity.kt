8a1a62f1b8ec664c1127d3b7282557cf96b09183
IlyaIIS <ilya1221221@mail.ru>
2023-05-09 11:40:52 +0500

    Работа над настройками генераторов

--- app/src/main/java/com/example/wallpapergenerator/GenerationActivity.kt
+++ app/src/main/java/com/example/wallpapergenerator/GenerationActivity.kt
@@ -1,21 +1,91 @@
 package com.example.wallpapergenerator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isGone
-import androidx.core.view.isVisible
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.wallpapergenerator.databinding.ActivityGenerationBinding
+
 
 class GenerationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGenerationBinding
+    private lateinit var nextPixels: IntArray
+    private lateinit var mainImage: ImageView
+    private var isWaitForImage = false
+    private var isNextImageReady = false
+    private lateinit var parameters: ParametersHolder
+    private lateinit var settingsFragment: GenerationSettingsFragment
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGenerationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        parameters = ViewModelProvider(this)[ParametersHolder::class.java]
+        mainImage = binding.mainImage
+        parameters.currentGenerationType = intent.getSerializableExtra("Type") as GenerationType
+        updateGenerationName()
+
+        binding.nextImageGenerationButton.setOnTouchListener(object: OnSwipeTouchListener(this@GenerationActivity) {
+            override fun onSwipeLeft() {
+                println("Left")
+                parameters.currentGenerationType = ((parameters.currentGenerationType.toInt() + 1).mod(GenerationType.values().size)).toEnum()
+                updateGenerationName()
+                returnNewImage()
+                settingsFragment.updateParameters()
+            }
+            override fun onSwipeRight() {
+                println("Right")
+                parameters.currentGenerationType = ((parameters.currentGenerationType.toInt() - 1).mod(GenerationType.values().size)).toEnum()
+                updateGenerationName()
+                returnNewImage()
+                settingsFragment.updateParameters()
+            }
+            override fun onClick() {
+                onNextImageGenerationClick()
+            }
+        })
+
+        binding.settingsButton.setOnClickListener {
+            if (binding.settingsFragmentContainer.visibility == View.GONE) {
+                binding.settingsFragmentContainer.visibility = View.VISIBLE
+                binding.nextImageGenerationButton.isEnabled = false
+            } else {
+                binding.settingsFragmentContainer.visibility = View.GONE
+                binding.nextImageGenerationButton.isEnabled = true
+            }
+        }
+
+        settingsFragment = supportFragmentManager.findFragmentById(R.id.settingsFragmentContainer) as GenerationSettingsFragment
     }
 
-/*    fun onClickMenu(view: View) {
+    fun updateGenerationName() {
+        binding.generationName.text = when (parameters.currentGenerationType) {
+            GenerationType.Gradients -> "Градиент"
+            GenerationType.Shapes -> "Фигуры"
+            GenerationType.Noise -> "Шум"
+            GenerationType.Fractals -> "Фракталы"
+            else -> throw NotImplementedError()
+        }
+    }
+
+    inline fun <reified T : Enum<T>> Int.toEnum(): T {
+        return enumValues<T>().first { it.ordinal == this }
+    }
+
+    inline fun <reified T : Enum<T>> T.toInt(): Int {
+        return this.ordinal
+    }
+
+/*    fun onClickMenu(view: View) { //замена фрагмента
         val fragmentMenu = PopupMenuFragment()
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         if (isMenuOpened) {
@@ -26,11 +96,135 @@ class GenerationActivity : AppCompatActivity() {
         }
         fragmentTransaction.commit()
     }*/
+
+    fun onNextImageGenerationClick() {
+        println("Generate next image")
+        returnNewImage()
+    }
+
     fun onClickMenu(view: View) {
-        if (binding.menuContainer.isVisible) {
-            binding.menuContainer.visibility = View.GONE
-        } else {
-            binding.menuContainer.visibility = View.VISIBLE
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    fun onExpandClick(view: View) {
+        println("Click no there")
+    }
+
+    private fun returnNewImage() {
+        fun getPixels(): IntArray {
+            return when (parameters.currentGenerationType) {
+                GenerationType.Gradients -> ImageGenerator.generateGradient(mainImage.width, mainImage.height, parameters.gradientParameters)
+                GenerationType.Shapes -> ImageGenerator.generateShapes(mainImage.width, mainImage.height)
+                GenerationType.Noise -> ImageGenerator.generateSinNoise(mainImage.width, mainImage.height, parameters.noiseParameters)
+                GenerationType.Fractals -> ImageGenerator.generateFractal(mainImage.width, mainImage.height, parameters.fractalParameters)
+                else -> throw NotImplementedError()
+            }
         }
+
+        fun startGeneration() {
+            Thread {
+                val pixels = getPixels()
+
+                if (isWaitForImage) {
+                    binding.generationIndicator.setCardBackgroundColor(Color.rgb(255, 105, 0))
+                    isWaitForImage = false
+                    runOnUiThread {
+                        drawPixels(pixels)
+                    }
+                    startGeneration()
+                } else {
+                    binding.generationIndicator.setCardBackgroundColor(Color.TRANSPARENT)
+                    isNextImageReady = true
+                    nextPixels = pixels
+                }
+            }.start()
+        }
+
+        if (!::nextPixels.isInitialized) {
+            binding.generationIndicator.setCardBackgroundColor(Color.RED)
+            nextPixels = getPixels()
+            isNextImageReady = true
+        }
+
+        if (isNextImageReady) {
+            println("Draw!!")
+            drawPixels(nextPixels)
+            isNextImageReady = false
+            binding.generationIndicator.setCardBackgroundColor(Color.rgb(255, 105, 0))
+            startGeneration()
+        } else {
+            println("Overwhelmed!!")
+            isWaitForImage = true
+            binding.generationIndicator.setCardBackgroundColor(Color.RED)
+        }
+    }
+
+    private fun drawPixels(pixels: IntArray) {
+        val bitmap = Bitmap.createBitmap(mainImage.width, mainImage.height, Bitmap.Config.ARGB_8888)
+        bitmap.setPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+        mainImage.setImageBitmap(bitmap)
+    }
+
+    data class NoiseParameters(
+        var isLevelCountRandom: Boolean,
+        var levelCount: Int,
+        var isTopColorRandom: Boolean,
+        var topColor: Int,
+        var isBottomColorRandom: Boolean,
+        var bottomColor: Int,
+    )
+
+    data class GradientParameters(
+        var gradientType: ImageGenerator.Companion.GradientType,
+        var isColorsRandom: Boolean,
+        var minColorsCount: Int,
+        var maxColorsCount: Int,
+        var colorsCount: Int,
+        var colors: IntArray
+    )
+
+    data class FractalParameters(
+        var fractalType: ImageGenerator.Companion.FractalType,
+        var coloringType: ImageGenerator.Companion.FractalColoringType,
+        var depth: Int,
+        var isOffsetRandom: Boolean,
+        var offsetX: Int,
+        var offsetY: Int,
+        var isZoomRandom: Boolean,
+        var zoom: Int,
+        var isTopColorRandom: Boolean,
+        var topColor: Int,
+        var isBottomColorRandom: Boolean,
+        var bottomColor: Int,
+    )
+
+    class ParametersHolder : ViewModel() {
+        lateinit var currentGenerationType: GenerationType
+        val noiseParameters = NoiseParameters(
+            true, 4,
+            true, Color.RED,
+            true, Color.BLUE)
+        val gradientParameters = GradientParameters(
+            ImageGenerator.Companion.GradientType.StrictLine,
+            true,
+            3,
+            7,
+            4,
+            intArrayOf(Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW)
+        )
+        var fractalParameters = FractalParameters(
+            ImageGenerator.Companion.FractalType.JuliaSet,
+            ImageGenerator.Companion.FractalColoringType.Module,
+            32,
+            true,
+            50,
+            50,
+            true,
+            0,
+            true,
+            Color.rgb(0, 200, 255),
+            true,
+            Color.GRAY
+        )
     }
 }
--- app/src/main/java/com/example/wallpapergenerator/MainActivity.kt
+++ app/src/main/java/com/example/wallpapergenerator/MainActivity.kt
@@ -4,14 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.wallpapergenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.generationButtonGradients.setOnClickListener {
+            goToGenerationAction(GenerationType.Gradients)
+        }
+        binding.generationButtonShapes.setOnClickListener {
+            goToGenerationAction(GenerationType.Shapes)
+        }
+        binding.generationButtonNoise.setOnClickListener {
+            goToGenerationAction(GenerationType.Noise)
+        }
+        binding.generationButtonFractals.setOnClickListener {
+            goToGenerationAction(GenerationType.Fractals)
+        }
     }
 
-    fun onGeneratorTypeClicked(view: View) {
-        startActivity(Intent(this, GenerationActivity::class.java))
+    fun goToGenerationAction(generationType: GenerationType) {
+        val intent = Intent(this, GenerationActivity::class.java)
+        intent.putExtra("Type", generationType)
+        startActivity(intent)
     }
 }
